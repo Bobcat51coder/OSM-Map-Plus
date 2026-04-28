f247732e6e829962023e94a2719a3d8a5cd37eca
Bobcat51coder <blesgourgues@yahoo.fr>
2026-04-28 17:34:50 +0200

    Add files via upload

---
+++ language/en_uk/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// Language: English
+$lang['Map']                = 'Map';
+$lang['Album']              = 'Album';
+$lang['All albums']         = 'All albums';
+$lang['Search']             = 'Search';
+$lang['Search a location...'] = 'Search a location…';
+$lang['Fit all markers']    = 'Fit all markers';
+$lang['Toggle photo list']  = 'Toggle photo list';
+$lang['Location']           = 'Location';
+$lang['View on OSM']        = 'View on OpenStreetMap';
+$lang['photos displayed']   = 'photos displayed';
+$lang['total']              = 'total';
+$lang['Loading...']         = 'Loading…';
+$lang['Recentrer'] = 'Fit';
+$lang['Liste'] = 'List';
+$lang['Créer album'] = 'Create album';
